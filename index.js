210182ddd987bae570d70554d8f648e07ee5b9c7
ibragimjon <ibrohimbiznesmen@gmail.con>
2022-04-06 18:37:07 +0500

    hamma narsa qoshildi

---
+++ index.js
@@ -0,0 +1,72 @@
+const drums = document.querySelectorAll('.drum')
+
+drums.forEach((drum) => {
+    drum.addEventListener('click', () => {
+        const buttonText = drum.textContent
+        playBaraban(buttonText)
+        animationButton (buttonText)
+    })
+})  
+
+// KEYDOWN
+
+
+document.addEventListener('keydown', (e) => {
+    const keyboard = e.key
+    playBaraban(keyboard)
+    animationButton (keyboard)
+})
+
+
+    
+function playBaraban (key) {
+    switch(key){
+        case 'w':
+            const crash = new Audio ('./sounds/crash.mp3')
+            crash.play()
+            break
+
+        case 'a' :
+            const kickBass = new Audio ('./sounds/kick-bass.mp3')
+            kickBass.play()
+            break
+
+        case 's' :
+            const snare = new Audio ('./sounds/snare.mp3')
+            snare.play()
+           break
+
+        case 'd' :
+            const tom1 = new Audio ('./sounds/tom-1.mp3')
+            tom1.play()
+             break
+
+        case 'j' :
+            const tom2 = new Audio ('./sounds/tom-2.mp3')
+            tom2.play()
+            break
+
+        case 'k':
+            const tom3 = new Audio ('./sounds/tom-3.mp3')
+            tom3.play()
+            break
+
+        case 'l':
+            const tom4 = new Audio ('./sounds/tom-4.mp3')
+            tom4.play()
+            break
+    }
+}
+
+
+
+function animationButton (key){
+    const salom = document.querySelector(`.${key}`)
+    salom.classList.add('pressed')
+
+    setTimeout(() => {
+        salom.classList.remove('pressed')
+
+    },200)
+}
+
